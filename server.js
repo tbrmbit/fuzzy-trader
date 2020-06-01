10abe7dadd83001826146682050c75183cb5a3b4
Thiago <tbrm.bit@gmail.com>
2020-06-01 16:13:25 -0300

    Improvements on server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.static('./dist/fuzzy-trader'));
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', {root: 'dist/fuzzy-trader/'});
+  res.sendFile(path.join(__dirname, '/dist/angular-tour-of-heroes/index.html'));
 });
 app.listen(process.env.PORT || 8080);
