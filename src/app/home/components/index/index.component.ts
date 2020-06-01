d8989bb0b8e9d0384f321f46b3134f206c0bfd23
Thiago <tbrm.bit@gmail.com>
2020-06-01 09:52:38 -0300

    Created services to get blockchain data

--- src/app/home/components/index/index.component.ts
+++ src/app/home/components/index/index.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BlockchainService } from '../../services/blockchain.service';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { Tricker } from '../../services/models/tricker';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, OnDestroy {
+  tricker$: Subscription;
+  btc$: Subscription;
 
-  constructor() { }
+  constructor(
+    public blockchainService: BlockchainService
+  ) { }
 
   ngOnInit(): void {
     console.log('here');
+    this.getBlockList();
+    this.convertBtc('USD', '554');
+  }
+
+  ngOnDestroy(): void {
+    this.tricker$.unsubscribe();
+  }
+
+  public getBlockList() {
+    this.tricker$ = this.blockchainService.getBicoinPriceList().subscribe(data => console.log(data));
+  }
+
+  public convertBtc(currency: string, value: string) {
+    this.btc$ = this.blockchainService.getBtcToPrice(currency, value).subscribe(data => console.log(data));
   }
 
 }
---
+++ src/app/home/services/blockchain.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { Tricker } from './models/tricker';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlockchainService {
+  private URL_BLOCKCHAIN = 'https://blockchain.info/ticker';
+  private URL_TOBTC = 'https://blockchain.info/tobtc';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public getBicoinPriceList(): Observable<Tricker> {
+    return this.http.get<Tricker>(this.URL_BLOCKCHAIN);
+  }
+
+  public getBtcToPrice(currency: string, value: string): Observable<any> {
+    const options = { params: this.toHttpParams({currency: currency, value: value}) };
+    return this.http.get<any>(this.URL_TOBTC, options).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: Response | any) {
+    console.error(error.error || error);
+    return throwError(error.error);
+  }
+
+  private toHttpParams(obj: Object): HttpParams {
+    return Object.getOwnPropertyNames(obj)
+      .reduce((p, key) => p.set(key, obj[key]), new HttpParams());
+  }
+}
+
